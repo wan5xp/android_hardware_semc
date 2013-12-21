2264e0ab08820c916476738c9983b5738e328958
munjeni <sharecard17@gmail.com>
2013-12-21 21:26:44 +0100

    update android.mk

--- mali/Android.mk
+++ mali/Android.mk
@@ -1,2 +1,8 @@
-LOCAL_PATH := $(call my-dir)
-include $(call all-subdir-makefiles)
+mali_modules := egl mali ump gralloc
+
+MALI_MAKEFILES := $(call all-named-subdir-makefiles,$(mali_modules))
+
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+include $(MALI_MAKEFILES)

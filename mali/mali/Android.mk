509dc838230459a5ed93a011a31dedbcc6f90dc4
munjeni <sharecard17@gmail.com>
2013-12-21 21:20:11 +0100

    preparation for new Mali (version r3p2-01rel3)

---
+++ mali/mali/Android.mk
@@ -0,0 +1,10 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_ARM_MODE := arm
+LOCAL_MODULE := libMali
+LOCAL_SRC_FILES := libMali.so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)

06223df52247da39c9a0f536d1166cba3029898c
Andrewas <andrewas2009@gmail.com>
2014-01-11 00:56:13 +0200

    Delete mali and refresh patches

--- mali/Android.mk
+++
@@ -1,8 +0,0 @@
-mali_modules := egl mali ump gralloc
-
-MALI_MAKEFILES := $(call all-named-subdir-makefiles,$(mali_modules))
-
-LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
-
-include $(MALI_MAKEFILES)

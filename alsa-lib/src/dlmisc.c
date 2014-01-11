826509d6b9edfa95cdacd7c3cd58c5a9ff7f3b0e
Andrewas <andrewas2009@gmail.com>
2014-01-11 16:16:59 +0200

    Update dlmisc.c

--- alsa-lib/src/dlmisc.c
+++ alsa-lib/src/dlmisc.c
@@ -57,9 +57,13 @@ void *snd_dlopen(const char *name, int mode)
 #ifdef ANDROID
 		return RTLD_DEFAULT;
 #else
-		Dl_info dlinfo;
-		if (dladdr(snd_dlopen, &dlinfo) > 0)
-			name = dlinfo.dli_fname;
+		static const char * self = NULL;
+		if (self == NULL) {
+			Dl_info dlinfo;
+			if (dladdr(snd_dlopen, &dlinfo) > 0)
+				self = dlinfo.dli_fname;
+		}
+		name = self;
 #endif
 	}
 #endif
